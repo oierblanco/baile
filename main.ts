66617da808ae3c8ff2c9b16c19ebc4adc954464a
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-05-11 14:12:13 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,35 @@
+input.onButtonPressed(Button.A, function () {
+    led2 = 1
+    for (let index = 0; index < 4; index++) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 75)
+        basic.pause(500)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 75)
+        basic.pause(500)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(100)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 75)
+        basic.pause(275)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(100)
+    }
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+    basic.pause(2150)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.pause(100)
+})
+let led2 = 0
+DFRobotMaqueenPlus.I2CInit()
+led2 = 0
+basic.forever(function () {
+    while (led2 == 1) {
+        for (let index = 0; index < 4; index++) {
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.CYAN)
+            basic.pause(100)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+            basic.pause(100)
+        }
+    }
+})
